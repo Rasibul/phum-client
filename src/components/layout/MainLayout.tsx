3db6a56e508cb47cea9c9cdaae90dce3802a4b70
Rasibul <rasibul179@gmail.com>
2025-01-06 23:01:54 +0600

    create a side nabvar menus

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,25 +1,33 @@
-import { Layout, Menu } from "antd";
+import { Layout, Menu, MenuProps } from "antd";
 const { Header, Content, Footer, Sider } = Layout;
-import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-import { createElement } from "react";
 
-const items = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  UserOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: createElement(icon),
-  label: `nav ${index + 1}`,
-}));
+const items: MenuProps["items"] = [
+  {
+    key: 1,
+    label: "Dashboard",
+  },
+  {
+    key: 2,
+    label: "Profile",
+  },
+  {
+    key: 3,
+    label: "User Management",
+    children: [
+      {
+        key: 4,
+        label: "Create User",
+      },
+      {
+        key: 5,
+        label: "show users",
+      },
+    ],
+  },
+];
 const MainLayout = () => {
   return (
-    <Layout>
+    <Layout style={{ height: "100vh" }}>
       <Sider
         breakpoint="lg"
         collapsedWidth="0"
